269888bdca049287d97c66cdf6889003047b2bd7
Amit Enbal <amitenbal@gmail.com>
2020-05-30 20:40:41 +0300

    finished alu64bit.sv, tell me how it works with your fas and alu1bit

--- alu64bit.sv
+++ alu64bit.sv
@@ -10,7 +10,39 @@ module alu64bit (
 
 // Put your code here
 // ------------------
-
+	logic [62:0] C_out;
+	
+	alu1bit ALU0(
+		.s(s[0]),
+		.cout(C_out[0]),
+		.op(op),
+		.cin(cin),
+		.a(a[0]),
+		.b(b[0])
+	);
+	alu1bit ALU63(
+		.s(s[63]),
+		.cout(cout),
+		.op(op),
+		.cin(C_out[62]),
+		.a(a[63]),
+		.b(b[63])
+	);
+		
+	genvar i;
+	generate
+		for(i=1;i<63;i++)
+		begin
+		alu1bit ALU(
+			.s(s[i]),
+			.cout(C_out[i]),
+			.op(op),
+			.cin(C_out[i-1]),
+			.a(a[i]),
+			.b(b[i])
+		);
+		end
+	endgenerate
 
 // End of your code
 
