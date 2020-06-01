0edee75ac2b7c0ff11c3b8ba40addb0298565507
daniellachino <daniel.lachino+github@gmail.com>
2020-06-01 15:59:21 +0300

    Added fas_testinformal and rewrote fas_test

--- fas_test.sv
+++ fas_test.sv
@@ -3,36 +3,24 @@ module fas_test;
 
 // Put your code here
 // ------------------
-logic a,b,c,d;
-logic [3:0] vec;
-assign {d,c,b,a} = vec;
+logic a,b,cin,ans;
 logic sum;
 logic carry_out= 1;
-logic sum_error = 1;
-logic carry_error = 1;
-fas UUT(vec[0],vec[1],vec[2],vec[3],sum,carry_out);
-int i = 0;
-logic [0:15] cout_truth_table  = 16'b0010101100010111;
-logic [0:15] sum_truth_table  = 16'b0110100101101001;
-logic carry_test;
-logic sum_test;
+
+fas UUT(a,b,cin,ans,sum,carry_out);
+
 initial begin
-    for (i = 0; i<=15; i++)begin
-        carry_test = cout_truth_table[i];
-        sum_test = sum_truth_table[i];
-        #50
-        vec = i;
-        #50;
-        if (sum != sum_test) sum_error =1;
-        else sum_error = 0;
-
-
-        if (carry_out != carry_test) carry_error =1;
-        else carry_error = 0;
-        
-        
-    end
+    a = 0;
+    b = 0;
+    cin = 0;
+    ans = 1;
+    #50;
+    cin = 1;
+    #50;
+    cin= 0;
+    #50;
     $stop;
+
 end
 
 // End of your code
